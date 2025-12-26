db7b98a083b286536d206d1aa9dade9dca981207
ABHINAVGARG05 <abhichhrp@gmail.com>
2025-12-26 19:21:27 +0530

    feat: first function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
     println!("Hello, world!");
+
+    let sentence: String = String::from("Abhinav Garg");
+    let first_word = get_first_word(sentence);
+
+    //let mut x:i32 = 10;
+
+    // for i in 0..1000 {
+    //     x = x + 100;
+    // }
+
+    println!("{}", first_word);
+}
+
+fn get_first_word(sentence: String) -> String {
+    let mut ans : String = String::from("");
+    for char in sentence.chars() {
+        ans.push_str(char.to_string().as_str());
+        if char == ' ' {
+            break;
+        }
+    }
+
+    return ans;
 }
